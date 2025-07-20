66b0f0e9b26bea852789df51ff683f93719c8590
jaum-fs <martins_joao@id.uff.br>
2025-07-20 15:58:29 -0300

    implementing validations for email and password, post request and style return msgs

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { ɵEmptyOutletComponent } from "@angular/router";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -12,4 +11,16 @@ export class DefaultLoginLayoutComponent {
   @Input() title: string = ""; 
   @Input() primaryBtnText: string = ""; 
   @Input() secondaryBtnText: string = ""; 
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output("submit") onSubmit = new EventEmitter;
+  @Output("navigate") onNavigate = new EventEmitter;
+
+
+  submit(){
+    this.onSubmit.emit();
+  }
+  
+  navigate(){
+    this.onNavigate.emit();
+  }
 }
--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input} from '@angular/core';
-import { FormControlName, ReactiveFormsModule } from '@angular/forms';
+import { Component, forwardRef, Input} from '@angular/core';
+import { ControlValueAccessor, FormControlName, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 
@@ -7,13 +7,44 @@ type InputTypes = "text" | "email" | "password"
   selector: 'app-primary-input',
   standalone: true,
   imports: [ReactiveFormsModule],
+  providers:[
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PrimaryInputComponent), 
+      multi: true
+    }
+],
   templateUrl: './primary-input.component.html',
   styleUrl: './primary-input.component.scss'
 })
-export class PrimaryInputComponent {
+export class PrimaryInputComponent implements ControlValueAccessor {
 
   @Input() type: InputTypes = "text";
-  @Input() formName: string = "";
   @Input() placeholder: string = "";
   @Input() label: string = "";
+  @Input() inputName: string = "";
+
+  value: string = ''
+  onChange: any = () => {}
+  onTouched: any = () => {}
+
+  onInput(event: Event){
+      const value = (event.target as HTMLInputElement).value
+      this.onChange(value)
+  }
+  
+  writeValue(obj: any): void {
+    this.value = obj
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched(fn);
+  }
+
+  setDisabledState(isDisabled: boolean): void {}
+
 }
