b8d21ac8a51039b5983bdbe65d6019d21cc44f63
jaum-fs <martins_joao@id.uff.br>
2025-07-21 23:39:29 -0300

    finish frontend

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   apiUrl: string = "http://localhost:8080/auth"
 
   constructor(private httpClient: HttpClient) { }
-  
+
   login(email: string, password: string) {
     return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", {email, password}).pipe(
        tap((value) => {
@@ -22,7 +22,7 @@ export class LoginService {
   }
 
   signup(name: string, email: string, password: string) {
-    return this.httpClient.post<LoginResponse>(this.apiUrl + "/signup", {name, email, password}).pipe(
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", {name, email, password}).pipe(
        tap((value) => {
           sessionStorage.setItem('auth-token', value.token);
           sessionStorage.setItem('username', value.name);
